37af04c3b70a6bd833ca53c9d2ff353a0ce7e725
Simone <info@simoneicardi.com>
2025-11-28 10:47:48 +0100

    hello JS

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// write line (deprecato e poco utile)
+// document.writeln('Hello World!');
+
+// alert (utile per debug e poco altro)
+// alert('Hello World!');
+
+// DOM manipulation (modifica utile e ricorrente)
+document.getElementById('mio_id').innerHTML = 'Hello World!';
+
+// log
+console.log("Hello World, ma solo per gli sviluppaoti!!");
