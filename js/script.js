5715dc3f0e126021bb0cff441d1af78994288d3f
Simone <info@simoneicardi.com>
2025-12-02 11:00:12 +0100

    es. for con parte di DOM

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-// stampiamo in log i numeri da 1 a 100
+const miaLIsta = document.getElementById("lista_output");
+
+console.log(miaLIsta);
+
+
+// stampiamo in log i numeri da 100 a 1
 
 // console.log(0,1,2,3);
 
 //  usiamo un ciclo FOR per farlo
-for (let i = 50; i <= 150; i++) {
-    console.log(i);
+for (let i = 99; i >= 1; i--) {
+    if (i % 2 === 1) { // se il numero è dispari
+        console.log(i);
+        // miaLIsta.innerHTML = miaLIsta.innerHTML + `<li> il numero è ${i} </li>`;
+        miaLIsta.innerHTML += `<li> il numero è ${i} </li>`;
+
+    }
+    // console.log(i);
+
 }
+
+console.log("fine ciclo");
+
