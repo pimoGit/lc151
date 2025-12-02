6c62d7c61c872b6c51b6406d2109563b913b130d
Simone <info@simoneicardi.com>
2025-12-02 11:52:24 +0100

    es. differenze for while doWhile

--- js/script.js
+++ js/script.js
@@ -21,3 +21,32 @@ for (let i = 99; i >= 1; i--) {
 
 console.log("fine ciclo");
 
+
+for (let index = 0; index < 10; index++) {
+    // istruzioni da ripetere
+    console.log(index);
+
+}
+
+// console.log(index);
+
+
+let index = 0;
+while (index < 10) {
+    // istruzioni da ripetere
+    console.log(index);
+    // istruzione che ci porta ad uscire dal ciclo
+    index++;
+}
+console.log("il valore di index fuori dal ciclo è: ", index);
+
+
+
+while (condizione) {
+
+    // istruzione che ad un certo punto porterà a false la condizione
+}
+
+do {
+    // fai quello che devi dal ciclo almeno una volta
+} while (condition);
