4c7ad821b1e03bda6bc76bcf7736cf7f73db15ab
Simone <info@simoneicardi.com>
2025-12-03 11:01:44 +0100

    es. uso metodi JS su array

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
-// creazione dell'array iniziale
-const giocattoli = ["Orsetto", "Trenino"];
-const numeri = [1, 2, "dfsdf", true];
-const booleani = [true, false, true];
+const cities = ['London', 'Shanghai', 'New York', 'Delhi', 'Kuala Lumpur'];
+const names = [];
+const numbers = [1, 2, 3];
+const colours = ['Red', 'Blue', 'Yellow'];
+const keys = ['q', 'w', 'e', 'r', 't', 'y'];
+const countries = ['Bolivia', 'Jordan', 'Greenland'];
+const fruits = ['Apple', 'Orange', 'Pear'];
 
-giocattoli.push("Bambola")
+// 1. Aggiungi Fred all'array names
+names.push("Fred");
+console.log(names);
 
-// giocattoli = ["Orsetto", "Trenino"];
+// 2. Aggiungi 4 alla fine dell'array numbers
+numbers.push(4);
+console.log(numbers);
 
+// 3. Aggiungi Rio all'inizio dell'array cities
+cities.unshift("Rio");
+console.log(cities);
 
-console.log(giocattoli);
+// 4. Rimuovi il primo colore dall'array colours
+colours.shift();
+console.log(colours);
 
+// 5. Rimuovi l'ultimo elemento dall'array keys
+keys.pop();
+console.log(keys);
+
+// 6. Rimuovi Jordan dall'array countries
+// mi ricavo l'indice di posizione dell'elemento
+const index = countries.indexOf("Jordan");
+console.log(index);
+
+// lo uso un splice per cancellarlo
+countries.splice(index, 1);
+console.log(countries);
 
 
