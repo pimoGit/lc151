abd9188c8167a1dd45d9bd320a6e1566369393ae
Simone <info@simoneicardi.com>
2025-12-03 11:47:30 +0100

    es. traversing array completo con anche oputput pagina

--- js/script.js
+++ js/script.js
@@ -1,38 +1,31 @@
+const mioOutput = document.getElementById("lista_output");
+
+
 const cities = ['London', 'Shanghai', 'New York', 'Delhi', 'Kuala Lumpur'];
-const names = [];
-const numbers = [1, 2, 3];
-const colours = ['Red', 'Blue', 'Yellow'];
-const keys = ['q', 'w', 'e', 'r', 't', 'y'];
-const countries = ['Bolivia', 'Jordan', 'Greenland'];
-const fruits = ['Apple', 'Orange', 'Pear'];
 
-// 1. Aggiungi Fred all'array names
-names.push("Fred");
-console.log(names);
 
-// 2. Aggiungi 4 alla fine dell'array numbers
-numbers.push(4);
-console.log(numbers);
+let gruppoLi = "";
 
-// 3. Aggiungi Rio all'inizio dell'array cities
-cities.unshift("Rio");
-console.log(cities);
+// traversiamo tutto l'array con un ciclo FOR
+for (let i = 0; i < cities.length; i++) {
 
-// 4. Rimuovi il primo colore dall'array colours
-colours.shift();
-console.log(colours);
+    let cityIesima = cities[i];
 
-// 5. Rimuovi l'ultimo elemento dall'array keys
-keys.pop();
-console.log(keys);
+    // console.log(cityIesima);
 
-// 6. Rimuovi Jordan dall'array countries
-// mi ricavo l'indice di posizione dell'elemento
-const index = countries.indexOf("Jordan");
-console.log(index);
+    gruppoLi += `<li class="mioli"> ${cityIesima} </li>`
 
-// lo uso un splice per cancellarlo
-countries.splice(index, 1);
-console.log(countries);
+    console.log(gruppoLi, "all'iterazione del for numero:", i);
+
+
+}
+
+
+console.log("fine del ciclo");
+
+
+mioOutput.innerHTML = gruppoLi;
+
+
 
 
