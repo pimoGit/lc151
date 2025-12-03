a647a7c4c8899ee4ec8d9da3c77ca96a0c287cf4
Simone <info@simoneicardi.com>
2025-12-03 10:34:00 +0100

    es. const reference Vs. value per variabili

--- js/script.js
+++ js/script.js
@@ -1,69 +1,14 @@
-const miaLIsta = document.getElementById("lista_output");
+// creazione dell'array iniziale
+const giocattoli = ["Orsetto", "Trenino"];
+const numeri = [1, 2, "dfsdf", true];
+const booleani = [true, false, true];
 
-console.log(miaLIsta);
+giocattoli.push("Bambola")
 
-
-// stampiamo in log i numeri da 100 a 1
-
-// console.log(0,1,2,3);
-
-//  usiamo un ciclo FOR per farlo
-for (let i = 99; i >= 1; i--) {
-    if (i % 2 === 1) { // se il numero è dispari
-        let contenutoUlMomentaneo = miaLIsta.innerHTML;
-        console.log(contenutoUlMomentaneo);
-        // miaLIsta.innerHTML = miaLIsta.innerHTML + `<li> il numero è ${i} </li>`;
-        miaLIsta.innerHTML += `<li> il numero è ${i} </li>`;
-    }
-    // console.log(i);
-
-}
-
-console.log("fine ciclo");
+// giocattoli = ["Orsetto", "Trenino"];
 
 
-for (let index = 0; index < 10; index++) {
-    // istruzioni da ripetere
-    console.log(index);
-
-}
-
-// console.log(index);
-
-
-let index = 0;
-while (index < 10) {
-    // istruzioni da ripetere
-    console.log(index);
-    // istruzione che ci porta ad uscire dal ciclo
-    index++;
-}
-console.log("il valore di index fuori dal ciclo è: ", index);
+console.log(giocattoli);
 
 
 
-while (condizione) {
-
-    // istruzione che ad un certo punto porterà a false la condizione
-}
-
-do {
-    // fai quello che devi dal ciclo almeno una volta
-} while (condition);
-
-
-// 1
-// 2
-// FIZZ
-// 4
-// BUZZ
-// FIZZ
-// 7
-// 8
-// FIZZ
-// BUZZ
-// 11
-// FIZZ
-// 13
-// 14
-// FIZZBUZZ
