09bf07250e9bce130f1ca2c7d8391834c621c509
Simone <info@simoneicardi.com>
2025-12-05 12:46:49 +0100

    es. centralizzazione/astrazione funzione per riutilizzo completo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 
-logCurrentTime();
+// logCurrentTime();
+console.log(getCurrentTime());
+
+
+
 
 // 10K LOC
 
 
-outputPageCurrentTime();
+// outputPageCurrentTime();
+document.getElementById("lista_output").innerHTML = getCurrentTime();
 
 
 // FUNZIONI
 
 // funzione che mi fa un log del tempo corrente
-function logCurrentTime() {
+function getCurrentTime() {
 
     // creo l'oggetto data da pc utente
     const now = new Date();
@@ -28,27 +33,8 @@ function logCurrentTime() {
     // mi compongo la stringa completa di output
     const currentTime = `${hour}:${minutes}:${seconds}`;
 
-    console.log(currentTime);
+    return currentTime;
 
 }
 
-// funzione di log tempo in pagina
-function outputPageCurrentTime() {
-    // creo l'oggetto data da pc utente
-    const now = new Date();
-    // console.log(now);
 
-    // mi creo la stringa con l'ora esatta
-    const hour = now.getHours();
-
-    // mi creo la stringa con i minuti esatti
-    const minutes = now.getMinutes();
-
-    // mi creo la stringa con i secondi esatti
-    const seconds = now.getSeconds();
-
-    // mi compongo la stringa completa di output
-    const currentTime = `${hour}:${minutes}:${seconds}`;
-
-    document.getElementById("lista_output").innerHTML = currentTime;
-}
