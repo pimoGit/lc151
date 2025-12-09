e8043a3e946212aedcafd3810dfdca992a114d8c
Simone <info@simoneicardi.com>
2025-12-09 11:26:30 +0100

    primo es. arrow function

--- js/script.js
+++ js/script.js
@@ -1,31 +1,25 @@
-var a;
-var b;
-var h;
-
-
-a = 0;
-b = "buongiorno"
-var c = "ciao";
-
-console.log("il valore di h prima dell'esecuzione della funzione è " + h);
-
-
-saluta();
-
-console.log("il valore di h dopo l'esecuzione della funzione è " + h);
-
-
-console.log("a is " + a);
-console.log("b is " + b);
+// versione arrow
+const myFuncion = () => console.log("ciao a tutti");
 
 
 
 
-function saluta() {
-    h = 10;
-    var d = "asd";
-    console.log("a is " + a);
-    var b = a + 1;
-    console.log("b is " + b);
-    console.log(c);
-}
+
+myFuncion();
+
+
+
+
+
+
+// FUNZIONI
+// versione tradizionale
+// function myFuncion() {
+//     console.log("ciao a tutti");
+// }
+
+
+
+
+
+
