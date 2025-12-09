fc33664ca649b21ec0450359f05ea49b83ddccd3
Simone <info@simoneicardi.com>
2025-12-09 10:51:06 +0100

    es. scope variabili

--- js/script.js
+++ js/script.js
@@ -1,40 +1,25 @@
-
-// logCurrentTime();
-console.log(getCurrentTime());
-
-
+var a;
+var b;
+var h;
 
 
-// 10K LOC
+a = 0;
+b = "buongiorno"
+var c = "ciao";
+
+saluta();
+
+console.log("a is " + a);
+console.log("b is " + b);
 
 
-// outputPageCurrentTime();
-document.getElementById("lista_output").innerHTML = getCurrentTime();
 
 
-// FUNZIONI
-
-// funzione che mi fa un log del tempo corrente
-function getCurrentTime() {
-
-    // creo l'oggetto data da pc utente
-    const now = new Date();
-    // console.log(now);
-
-    // mi creo la stringa con l'ora esatta
-    const hour = now.getHours();
-
-    // mi creo la stringa con i minuti esatti
-    const minutes = now.getMinutes();
-
-    // mi creo la stringa con i secondi esatti
-    const seconds = now.getSeconds();
-
-    // mi compongo la stringa completa di output
-    const currentTime = `${hour}:${minutes}:${seconds}`;
-
-    return currentTime;
-
+function saluta() {
+    h = 10;
+    var d = "asd";
+    console.log("a is " + a);
+    var b = a + 1;
+    console.log("b is " + b);
+    console.log(c);
 }
-
-
