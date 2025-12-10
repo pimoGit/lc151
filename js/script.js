f573cf35bb016750fb8814c0fc4d60bffc34959e
Simone <info@simoneicardi.com>
2025-12-10 12:39:06 +0100

    es. gestione manuale di cambiamenti

--- js/script.js
+++ js/script.js
@@ -5,13 +5,18 @@ const paragrafo = document.getElementById("outputPage");
 paragrafo.addEventListener("click",
 
     function () {
-        // if (paragrafo.classList.contains("changeColors")) {
-        //     // rimuovo la calsse
-        //     paragrafo.classList.remove("changeColors");
-        // } else {
-        //     // aggiungo la classe
-        //     paragrafo.classList.add("changeColors");
-        // }
-        paragrafo.classList.toggle("changeColors");
+        if (paragrafo.classList.contains("changeColors")) {
+            // rimuovo la calsse
+            paragrafo.classList.remove("changeColors");
+            // rimetto contenuto originale
+            paragrafo.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae explicabo accusamus aliquid veniam quos dolore velit pariatur ad iste illum maiores consectetur sit assumenda officia quo, dolor, quas doloribus! Molestias.";
+
+        } else {
+            // aggiungo la classe
+            paragrafo.classList.add("changeColors");
+            // cambio anche contenuto paragrafo
+            paragrafo.innerText = "altro contenuto";
+        }
+        // paragrafo.classList.toggle("changeColors");
     }
 );
