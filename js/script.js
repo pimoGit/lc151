7516b494303ea8a88d9709fa97c48a09d2ab278f
Simone <info@simoneicardi.com>
2025-12-10 12:16:49 +0100

    es. DOM manipulation con condizioni

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
-const button = document.querySelector('button');
+/* 
+Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
+Sulla pagina html deve apparire “Ciao <nome>”, 
+il colore del nome deve essere azzurro o rosa a seconda del sesso inserito */
 
-function logNumber(num) {
-    console.log('Il numero è: ' + num)
-}
 
-button.addEventListener('click', function () {
-    logNumber(3);
-});
+
+// fase di preparazione
+// selezione elemento output
+const pOutput = document.getElementById("outputPage");
+
+// Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
+const nomeUser = prompt("inserisci il tuo nome");
+const genere = prompt("scegli tra M e F");
+
+console.log(pOutput, nomeUser, genere);
+
+// Sulla pagina html deve apparire “Ciao <nome>”, 
+pOutput.innerText = nomeUser;
+
+// il colore del nome deve essere azzurro o rosa a seconda del sesso inserito */
+// let myColor;
+
+// if (genere === "F") {
+//     myColor = "pink";
+// } else {
+//     myColor = "blue";
+// }
+
+const myColor = genere === "F" ? "pink" : "blue";
+
+pOutput.style.color = myColor;
