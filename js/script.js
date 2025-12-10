92831b683e8373a3b74c347ef354430ab52e8b5e
Simone <info@simoneicardi.com>
2025-12-10 12:27:46 +0100

    es. uso evento e arrow CB function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,5 @@
-/* 
-Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
-Sulla pagina html deve apparire “Ciao <nome>”, 
-il colore del nome deve essere azzurro o rosa a seconda del sesso inserito */
+// selezione elemento di interazioe
+const paragrafo = document.getElementById("outputPage");
 
-
-
-// fase di preparazione
-// selezione elemento output
-const pOutput = document.getElementById("outputPage");
-
-// Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
-const nomeUser = prompt("inserisci il tuo nome");
-const genere = prompt("scegli tra M e F");
-
-console.log(pOutput, nomeUser, genere);
-
-// Sulla pagina html deve apparire “Ciao <nome>”, 
-pOutput.innerText = nomeUser;
-
-// il colore del nome deve essere azzurro o rosa a seconda del sesso inserito */
-// let myColor;
-
-// if (genere === "F") {
-//     myColor = "pink";
-// } else {
-//     myColor = "blue";
-// }
-
-const myColor = genere === "F" ? "pink" : "blue";
-
-pOutput.style.color = myColor;
+// gestisco evennto
+paragrafo.addEventListener("click", () => paragrafo.classList.add("changeColors"));
