e7ffc8bd549a969f4876bf756faa33dd9077bc26
Simone <info@simoneicardi.com>
2025-12-10 12:56:27 +0100

    chiusura menù

--- js/script.js
+++ js/script.js
@@ -3,11 +3,21 @@
 const menuButton = document.getElementById("menu-toggle");
 // selezioniamo elemento di mobile
 const mobileMenu = document.querySelector(".mobile-menu");
+// seleziono elemento di chiusura
+const closeButton = document.querySelector(".close");
 
 // gestione apertura menù
 menuButton.addEventListener("click",
     function () {
         // apri sto maledetto menù
         mobileMenu.classList.add("active");
+    }
+)
+
+// gestione chiusura menù
+closeButton.addEventListener("click",
+    function () {
+        // chiudi sto maledetto menù
+        mobileMenu.classList.remove("active");
     }
 )
