2873e32995d653799be4fc7b8d00919cb98268a7
Simone <info@simoneicardi.com>
2025-12-11 11:04:26 +0100

    es. value su input con numero

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 const field = document.getElementById('name');
 const button = document.querySelector('button');
+const numberInput = document.getElementById("numero");
 
 // se proviamo a leggere qui, sarà sempre vuoto
-const nameUser = field.value; //
+// const nameUser = field.value; //
 
-console.log("primo tentativo fallace di ritorno valore: ", nameUser);
+// console.log("primo tentativo fallace di ritorno valore: ", nameUser);
 
 // let nameUserInterno;
 
 button.addEventListener('click', () => {
     const nameUserInterno = field.value;
-    console.log("secondo tentativo da evento click, CB", nameUserInterno);
+    const numeroUser = parseInt(numberInput.value);
+    console.log("secondo tentativo da evento click, CB", nameUserInterno, numeroUser);
 });
 
 
