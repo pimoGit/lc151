5fa3578f819460a5e83eb6d9fc09ec7a2fe6f063
Simone <info@simoneicardi.com>
2025-12-11 11:28:47 +0100

    es. aggiunta oputput e clear campi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const field = document.getElementById('name');
 const button = document.querySelector('button');
 const numberInput = document.getElementById("numero");
+const paragrafo = document.querySelector(".output");
 
 // se proviamo a leggere qui, sarà sempre vuoto
 // const nameUser = field.value; //
@@ -13,6 +14,11 @@ button.addEventListener('click', () => {
     const nameUserInterno = field.value;
     const numeroUser = parseInt(numberInput.value);
     console.log("secondo tentativo da evento click, CB", nameUserInterno, numeroUser);
+    // scriviamo output
+    paragrafo.innerText = nameUserInterno + numeroUser;
+    // pulire i campi
+    field.value = "";
+    numberInput.value = "";
 });
 
 
