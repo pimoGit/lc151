f461e562cf5a67a8429d993882feb51da9bbe378
Simone <info@simoneicardi.com>
2025-12-12 10:30:38 +0100

    es. countdown con setItreval clear

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-console.log("sei nel file giusto");
+// generiamo un conutdown di capodanno
 
+// selezioniamo l'elemento di output
+const myOutput = document.getElementById("show");
 
-setTimeout(myFunction, 1000);
+// variabile di inizio conteggio
+let seconds = 10;
 
-function myFunction() {
-    alert('Hello');
-}
+// // inseriamo output aggiornato secondi
+myOutput.innerText = seconds;
+
+// gestiamo ogni secondo l'aggiornamento dell'output
+const clock = setInterval(() => {
+    // SE siamo alla fine (secondi sono 0) - fermiamo il conteggio e diamo messaggio nuovo
+    if (seconds === 0) {
+        // blocco la ripetizione temporale
+        clearInterval(clock);
+        myOutput.innerText = "Bauo anno!!!!";
+    } else {
+        // decrementiamo seconds
+        seconds = seconds - 1;
+        // inseriamo output aggiornato secondi
+        myOutput.innerText = seconds;
+    }
+
+}, 1000)
+// ogni secondo scala una unità
