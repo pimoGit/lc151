14345ac679fcfca6d097957a9c20583294627a93
Simone <info@simoneicardi.com>
2025-12-12 12:32:58 +0100

    completo progress bar simulazione

--- js/script.js
+++ js/script.js
@@ -1,57 +1,53 @@
-// creiamo un cronometro
+// creiamo una simulazione di una barra di caricamento
+
 
 // PREPARAZIONE
 
-// selezioniamo tutti gli elementi di input e output di pagina
-// input
-const startButton = document.querySelector(".btn-success");
-const stopButton = document.querySelector(".btn-danger");
-// output
-const display = document.getElementById("display");
-
-// variabile globale per interval
-let timer;
+// slezioni I/O
+const message = document.getElementById("message");
+const progressBar = document.querySelector(".progress-bar");
+const uploadButton = document.getElementById("upload-button");
 
 
 
-// ELABORAZIONE (e OUTPUT)
-
-// -------
-// gestione dello start
-startButton.addEventListener("click", () => {
-
-    // gestire disable dei bottoni
-    startButton.disabled = true;
-    stopButton.disabled = false;
-
-    // gestiamo il timer
-    let seconds = 0;
-    display.innerText = seconds;
-
-    // interval con incremento dei secondi
-    timer = setInterval(function () {
-        // vado a incrementare secondi e aggiorno output
-        display.innerText = ++seconds;
-    }, 1000);
-
-});
 
 
-// -------
-// gestione dello stop
-stopButton.addEventListener("click", () => {
-    // gestire disable dei bottoni
-    startButton.disabled = false;
-    stopButton.disabled = true;
+// ELABORAZIONE/OUTPUT
 
-    // blocchiamo il timer
-    clearInterval(timer);
+// gestione del bottone di upload
+uploadButton.addEventListener("click", () => {
+
+    // blocco bottone (disabilitiamo) e cambiamo anche testo del bottone stesso
+    uploadButton.disabled = true;
+    uploadButton.innerText = "Uploading...";
+
+    // inizializzazione dei valori
+    let progress = 0;
+    let percentageValue = progress + "%"
+
+    // imposto O sia della barra che del testo al suo interno
+    progressBar.innerText = percentageValue;
+    progressBar.style.width = percentageValue;
+
+    // gestiamo il progresso dellla barra con incremento valori
+    const intervalPerc = setInterval(() => {
+        // SE sono alla fine
+        if (progress === 100) {
+            // fermare l'esecuzione (interval)
+            clearInterval(intervalPerc);
+            // e gestire O finale
+            message.classList.remove("d-none");
+            uploadButton.classList.add("d-none");
+        } else {
+            // ALTRIMENTI aggiorno contatore con relativo O
+            percentageValue = ++progress + "%"
+            // imposto O sia della barra che del testo al suo interno
+            progressBar.innerText = percentageValue;
+            progressBar.style.width = percentageValue;
+        }
+
+
+    }, 100);
 })
 
 
-
-// FUNZIONI
-function incrementTimer(conteggio) {
-    // vado a incrementare secondi e aggiorno output
-    display.innerText = ++conteggio;
-}
