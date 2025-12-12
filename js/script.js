3b8568a6ffcb67c2dd7945c1c77dc6fffc587c9c
Simone <info@simoneicardi.com>
2025-12-12 13:05:38 +0100

    es. funzione creare array di numeri random univoci

--- js/script.js
+++ js/script.js
@@ -1,53 +1,30 @@
-// creiamo una simulazione di una barra di caricamento
 
-
-// PREPARAZIONE
-
-// slezioni I/O
-const message = document.getElementById("message");
-const progressBar = document.querySelector(".progress-bar");
-const uploadButton = document.getElementById("upload-button");
+console.log(getArrRadnomNumInRangeTotEl(1, 50, 5));
 
 
 
 
+// funzione per creare un aray di tot numeri univoci
 
-// ELABORAZIONE/OUTPUT
+function getArrRadnomNumInRangeTotEl(minNUm, maxNUm, tot) {
 
-// gestione del bottone di upload
-uploadButton.addEventListener("click", () => {
+    const numArr = [];
 
-    // blocco bottone (disabilitiamo) e cambiamo anche testo del bottone stesso
-    uploadButton.disabled = true;
-    uploadButton.innerText = "Uploading...";
+    while (numArr.length < tot) {
+        const randomNum = genRandomNumInRange(minNUm, maxNUm);
 
-    // inizializzazione dei valori
-    let progress = 0;
-    let percentageValue = progress + "%"
-
-    // imposto O sia della barra che del testo al suo interno
-    progressBar.innerText = percentageValue;
-    progressBar.style.width = percentageValue;
-
-    // gestiamo il progresso dellla barra con incremento valori
-    const intervalPerc = setInterval(() => {
-        // SE sono alla fine
-        if (progress === 100) {
-            // fermare l'esecuzione (interval)
-            clearInterval(intervalPerc);
-            // e gestire O finale
-            message.classList.remove("d-none");
-            uploadButton.classList.add("d-none");
-        } else {
-            // ALTRIMENTI aggiorno contatore con relativo O
-            percentageValue = ++progress + "%"
-            // imposto O sia della barra che del testo al suo interno
-            progressBar.innerText = percentageValue;
-            progressBar.style.width = percentageValue;
+        if (!numArr.includes(randomNum)) {
+            numArr.push(randomNum);
         }
+    }
+
+    return numArr
+
+}
 
 
-    }, 100);
-})
 
-
+// funzione che genera random num tra min e max
+function genRandomNumInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
