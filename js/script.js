b707366f16baa034920749e073332f79a351ff7d
Simone <info@simoneicardi.com>
2025-12-15 12:45:00 +0100

    es. gestione elenco con totale

--- js/script.js
+++ js/script.js
@@ -1,75 +1,45 @@
-// defniamo il nostro array di oggetti classi
-const classi =
+/* 
+ * Crea 10 oggetti che rappresentano una zucchina, 
+ * indicandone per ognuno varietà, peso e lunghezza. 
+ * Calcola quanto pesano tutte le zucchine.
+ */
 
-    [
+//  * Crea 10 oggetti che rappresentano una zucchina, 
+//  * indicandone per ognuno varietà, peso e lunghezza.
 
-        {
-            'nome': 'Classe 1',
-            'numero_studenti': 10,
-            'studenti': ["Alessia", "Mattia"]
-        },
+const zucchine = [
+    { varieta: "Nera di Milano", peso: 250, lunghezza: 18 },
+    { varieta: "Romanesca", peso: 300, lunghezza: 22 },
+    { varieta: "Chiara di Faenza", peso: 280, lunghezza: 20 },
+    { varieta: "Tonda di Nizza", peso: 350, lunghezza: 12 },
+    { varieta: "Lunga Fiorentina", peso: 400, lunghezza: 30 },
+    { varieta: "Striata d'Italia", peso: 270, lunghezza: 19 },
+    { varieta: "Bianca Triestina", peso: 320, lunghezza: 24 },
+    { varieta: "Greyzini", peso: 290, lunghezza: 21 },
+    { varieta: "Cocozelle", peso: 360, lunghezza: 26 },
+    { varieta: "Nera di Milano", peso: 310, lunghezza: 23 }
+];
 
-        {
-            'nome': 'Classe 2',
-            'numero_studenti': 15
-        }
+//  * Calcola quanto pesano tutte le zucchine.
 
-    ];
+// settiamo una var per accumulare il totale dei pesi
+let sommaPesoZucchine = 0;
 
+// cicliamo la lista delle zucchine
+for (let index = 0; index < zucchine.length; index++) {
 
+    // estrapoliamo ogni volta una zucchina diversa
+    const zucchina = zucchine[index];
 
+    console.log(zucchina.peso);
+    // incrementiamo il peso totale
+    sommaPesoZucchine = sommaPesoZucchine + zucchina.peso;
 
+    console.log("il progresso della somma è: ", sommaPesoZucchine);
 
-// modifichiamo valore di una propietà
-// palla.diametro = 5; // DOT notation
-// const primaClasse = classi[0];
-
-classi[0].nome = "Classe 1 nuovo nome";
-
-// console.log(classi[0].nome);
-
-
-
-
-// mi faccio tornare il valore di tipo
-const numeStudetiClasse2 = classi[1].numero_studenti;
-// console.log(numeStudetiClasse2);
-
-
-// inserire una nuova prop
-classi[0].insegnate = "nuovo insegante";
-
-// ritornare il primo studente della prima classe
-const primoStudente = classi[0].studenti[0];
-// console.log(primoStudente);
-
-
-// inseriasco una nuova classe alla fine
-const nuovoObjClasse = {
-    'nome': 'Classe 3',
-    'numero_studenti': 25
-}
-
-classi.push(nuovoObjClasse);
-
-
-// console.log(classi);
-
-
-// se voglio ciclare l'oggetto per estrarre tutto quello che c'è
-for (let index = 0; index < classi.length; index++) {
-    const classeIesima = classi[index];
-    console.log(classeIesima);
-
-    // console.log("ilnome della classe è :", classeIesima.nome, "e ha tot parteciapnti", classeIesima.numero_studenti);
-
-    for (const key in classeIesima) {
-
-        const valoreClasse = classeIesima[key];
-
-        console.log(key, valoreClasse);
-
-    }
 
 }
 
+
+// stampiamo il peso totale
+console.log("il peso totale delle zucchine è: ", sommaPesoZucchine);
