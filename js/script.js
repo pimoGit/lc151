b399fc5816cade66f76b60f2abc0160326d87aaf
Simone <info@simoneicardi.com>
2025-12-15 10:59:57 +0100

    gestione base oggetto JS

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
-
-console.log(getArrRadnomNumInRangeTotEl(1, 50, 5));
-
-
-
-
-// funzione per creare un aray di tot numeri univoci
-
-function getArrRadnomNumInRangeTotEl(minNUm, maxNUm, tot) {
-
-    const numArr = [];
-
-    while (numArr.length < tot) {
-        const randomNum = genRandomNumInRange(minNUm, maxNUm);
-
-        if (!numArr.includes(randomNum)) {
-            numArr.push(randomNum);
-        }
-    }
-
-    return numArr
-
+// defniamo il nostro oggetto palla
+const palla = {
+    'colore': ["blue", "red"],
+    'tipo': "pallina da golf",
+    'diametro': 3
 }
 
 
+// modifichiamo valore di una propietà
+// palla.diametro = 5; // DOT notation
+palla["diametro"] = 5 // Brackets notation
 
-// funzione che genera random num tra min e max
-function genRandomNumInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+
+// mi faccio tornare il valore di tipo
+console.log(palla.colore[1]);// DOT notation
+console.log(palla["colore"]); // Brackets notation
+
+const nomeProp = prompt("inserisci il nome delle prop")
+
+// inserire una nuova prop
+palla.peso = 10; // DOT notation
+palla[nomeProp] = 10; // Brackets notation
+
+console.log(palla);
+
+// se voglio ciclare l'oggetto per estrarre tutto quello che c'è
+for (const chiave in palla) {
+    console.log(chiave, palla[chiave]);
 }
+
