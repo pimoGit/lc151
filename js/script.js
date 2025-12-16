20daf2b57cd6aa28d57c2a0a4d8e665f92203c16
Simone <info@simoneicardi.com>
2025-12-16 11:48:10 +0100

    setup iniziale e primo output test page member team

--- js/script.js
+++ js/script.js
@@ -1,31 +1,50 @@
-const zucchine = [
-    { varieta: 'Varietà 1', peso: 10, lunghezza: 4 },
-    { varieta: 'Varietà 2', peso: 13, lunghezza: 16 },
-    { varieta: 'Varietà 3', peso: 4, lunghezza: 23 },
-    { varieta: 'Varietà 4', peso: 11, lunghezza: 6 },
-    { varieta: 'Varietà 5', peso: 2, lunghezza: 17 },
-    { varieta: 'Varietà 6', peso: 5, lunghezza: 10 },
-    { varieta: 'Varietà 7', peso: 7, lunghezza: 9 },
-    { varieta: 'Varietà 8', peso: 3, lunghezza: 8 },
-    { varieta: 'Varietà 9', peso: 6, lunghezza: 27 },
-    { varieta: 'Varietà 10', peso: 14, lunghezza: 4 }
+const teamMembers = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    },
 ];
 
+// parte di setUp
+const outputTest = document.getElementById("team-list");
 
-const zucchine2 = [...zucchine];
+// mi creo var che accumulerà gli elementi di ouput
+let outputTotli = "";
 
+// output di prova ciclando l'array di oggetti
+for (let index = 0; index < teamMembers.length; index++) {
+    // estrapoliamo ogni volta un oggetto membro divero
+    const memberTeam = teamMembers[index];
 
-
-
-const nuovoArr = [];
-
-for (let index = 0; index < zucchine.length; index++) {
-    const oggettoZucchina = zucchine[index];
-
-    nuovoArr.push({ ...oggettoZucchina });
-
+    // andiamo ad ogni giro ad incrementare il contenuto dell'output
+    outputTotli = outputTotli + `<li>${memberTeam.name} ${memberTeam.role} ${memberTeam.image}</li>`
 }
 
-zucchine[0].varieta = "Varietà Nuova";
-
-console.log(zucchine, nuovoArr);
+// inseriamo la stringa completa che innerHTML trasformerà, nell'elemento di output
+outputTest.innerHTML = outputTotli;
