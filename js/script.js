bf1cd4fd72512e391e6ea00cd32b4587b1b6cefe
Simone <info@simoneicardi.com>
2025-12-16 12:32:41 +0100

    team membere completo con funzioni e stetup iniziale form

--- js/script.js
+++ js/script.js
@@ -32,19 +32,58 @@ const teamMembers = [
 ];
 
 // parte di setUp
-const outputTest = document.getElementById("team-list");
+// const outputTest = document.getElementById("team-list");
+const teamContainer = document.querySelector(".team-container");
 
-// mi creo var che accumulerà gli elementi di ouput
-let outputTotli = "";
+// selezione di elementi form
+const form = document.getElementById("member-form");
+const nameField = document.getElementById('name');
+const roleField = document.getElementById('role');
+const imageField = document.getElementById('image');
 
-// output di prova ciclando l'array di oggetti
-for (let index = 0; index < teamMembers.length; index++) {
-    // estrapoliamo ogni volta un oggetto membro divero
-    const memberTeam = teamMembers[index];
+// chiamo funzione per renderizzare la lista mebri
+rederTeam(teamMembers, teamContainer);
 
-    // andiamo ad ogni giro ad incrementare il contenuto dell'output
-    outputTotli = outputTotli + `<li>${memberTeam.name} ${memberTeam.role} ${memberTeam.image}</li>`
+
+
+// funzioni dell'APP
+
+// funzione che fa il rendering completo delle card dei membri del team
+function rederTeam(arraRef, outputEL) {
+    // mi creo var che accumulerà gli elementi di ouput
+    let cards = "";
+
+    // output di prova ciclando l'array di oggetti
+    for (let index = 0; index < arraRef.length; index++) {
+        // estrapoliamo ogni volta un oggetto membro divero
+        const memberTeam = arraRef[index];
+
+        // destrutturiamo l'oggetto
+        // const { name, image, role } = memberTeam;
+
+        // andiamo ad ogni giro ad incrementare il contenuto dell'output
+        cards += createMemberCard(memberTeam);
+    }
+
+    // inseriamo la stringa completa che innerHTML trasformerà, nell'elemento di output
+    outputEL.innerHTML = cards;
 }
 
-// inseriamo la stringa completa che innerHTML trasformerà, nell'elemento di output
-outputTest.innerHTML = outputTotli;
+// funzione che genera la card ricevendo oggetto dal quale prendere le info
+function createMemberCard(memebrObj) {
+
+    const card = `
+    <div class="team-card">
+            <div class="card-image">
+                <img src="img/${memebrObj.image}" alt="${memebrObj.name}" />
+            </div >
+        <div class="card-text">
+            <h3>${memebrObj.name}</h3>
+            <p>${memebrObj.role}</p>
+        </div>
+    </div >
+        `;
+
+    return card
+
+}
