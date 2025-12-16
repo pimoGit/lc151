e5054412d99d85323329518a9e93fb302eb60695
Simone <info@simoneicardi.com>
2025-12-16 10:10:46 +0100

    es. destructuring con anche alias

--- js/script.js
+++ js/script.js
@@ -1,45 +1,23 @@
-/* 
- * Crea 10 oggetti che rappresentano una zucchina, 
- * indicandone per ognuno varietà, peso e lunghezza. 
- * Calcola quanto pesano tutte le zucchine.
- */
-
-//  * Crea 10 oggetti che rappresentano una zucchina, 
-//  * indicandone per ognuno varietà, peso e lunghezza.
-
-const zucchine = [
-    { varieta: "Nera di Milano", peso: 250, lunghezza: 18 },
-    { varieta: "Romanesca", peso: 300, lunghezza: 22 },
-    { varieta: "Chiara di Faenza", peso: 280, lunghezza: 20 },
-    { varieta: "Tonda di Nizza", peso: 350, lunghezza: 12 },
-    { varieta: "Lunga Fiorentina", peso: 400, lunghezza: 30 },
-    { varieta: "Striata d'Italia", peso: 270, lunghezza: 19 },
-    { varieta: "Bianca Triestina", peso: 320, lunghezza: 24 },
-    { varieta: "Greyzini", peso: 290, lunghezza: 21 },
-    { varieta: "Cocozelle", peso: 360, lunghezza: 26 },
-    { varieta: "Nera di Milano", peso: 310, lunghezza: 23 }
-];
-
-//  * Calcola quanto pesano tutte le zucchine.
-
-// settiamo una var per accumulare il totale dei pesi
-let sommaPesoZucchine = 0;
-
-// cicliamo la lista delle zucchine
-for (let index = 0; index < zucchine.length; index++) {
-
-    // estrapoliamo ogni volta una zucchina diversa
-    const zucchina = zucchine[index];
-
-    console.log(zucchina.peso);
-    // incrementiamo il peso totale
-    sommaPesoZucchine = sommaPesoZucchine + zucchina.peso;
-
-    console.log("il progresso della somma è: ", sommaPesoZucchine);
-
-
+const person = {
+    firstName: 'Marco',
+    lastName: 'Lanci',
+    age: 37,
+    year: 1986
 }
+// const nomeUtente = person.firstName;
+// const cognomeUtente = person.lastName;
+// const etaUtente = person.age;
 
 
-// stampiamo il peso totale
-console.log("il peso totale delle zucchine è: ", sommaPesoZucchine);
+// destructuring
+const { firstName: nomeUtente, lastName: cognomeUtente, age: etaUtente } = person;
+
+console.log(nomeUtente, cognomeUtente, etaUtente);
+
+const outputCont = document.getElementById("container");
+
+const template = `<div>${nomeUtente}</div>
+<div>${cognomeUtente}</div>
+<div>${etaUtente}</div>`;
+
+outputCont.innerHTML = template;
