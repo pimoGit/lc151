55ea1a1898863578fdf3c34d4e098ff0d4a655fd
Simone <info@simoneicardi.com>
2025-12-18 10:27:27 +0100

    es. filter

--- js/script.js
+++ js/script.js
@@ -1,32 +1,11 @@
 const students = [
-    { firstName: 'Mario', lastName: 'Rossi' },
-    { firstName: 'Luigi', lastName: 'Verdi' },
-    { firstName: 'Margherita', lastName: 'Bianchi' }
+    { id: 1, name: 'Marco', year: 1 },
+    { id: 1, name: 'Silvia', year: 2 },
+    { id: 1, name: 'Ginevra', year: 1 }
 ];
 
-// RISULTATO: ['Mario Rossi', 'Luigi Verdi', 'Marghertita Bianchi']
-
-const names = students.map((studentObjet) => {
-    return `${studentObjet.firstName} ${studentObjet.lastName}`
+const firstYearStudents = students.filter((studentItem) => {
+    return studentItem.year === 1
 });
 
-console.log(names);
-
-// versione forEach
-const names2 = [];
-students.forEach((studentObjet2) => {
-    names2.push(`${studentObjet2.firstName} ${studentObjet2.lastName}`)
-});
-
-console.log(names2);
-
-// versione for tradizionale
-const names3 = [];
-for (let i = 0; i < students.length; i++) {
-
-    const studentObjet3 = students[i];
-
-    names3.push(`${studentObjet3.firstName} ${studentObjet3.lastName}`)
-}
-
-console.log(names3);
+console.log(firstYearStudents, students);
