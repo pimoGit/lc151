f2e5f94896755b6adc062837d3909b72dc499f57
Simone <info@simoneicardi.com>
2025-12-18 11:04:40 +0100

    es. utilizzo in dati completi

--- js/script.js
+++ js/script.js
@@ -1,27 +1,71 @@
-const people = [
-    { id: 4, name: 'Paolo', age: 32 },
-    { id: 3, name: 'Clelia', age: 34 },
+const students = [
+    {
+        id: 1,
+        name: 'Luca Bianchi',
+        age: 32,
+        userClass: '3C',
+        vote: 9
+    },
+    {
+        id: 2,
+        name: 'Andrea Rossi',
+        age: 18,
+        userClass: '4A',
+        vote: 7
+    },
+    {
+        id: 3,
+        name: 'Giovanni Verdi',
+        age: 33,
+        userClass: '5B',
+        vote: 7
+    },
+    {
+        id: 4,
+        name: 'Francesco Esposito',
+        age: 25,
+        userClass: '5B',
+        vote: 7
+    },
+    {
+        id: 5,
+        name: 'Matteo Ferri',
+        age: 57,
+        userClass: '5B',
+        vote: 10
+    },
 ];
 
-// trova la person con id 3
-const myPerson = people.find((person) => {
-    return person.id === 3;
+// Stampa in console i nomi di tutti gli studenti
+students.forEach((student) => {
+    console.log(student.name);
+
+})
+
+// Crea un array con la lista  di tutti i nomi degli studenti
+const studentsName = students.map((student) => {
+    return student.name
+})
+console.log(studentsName);
+
+// Crea un array che contenga solo gli studenti dai 30 anni in più
+const studentsOver30 = students.filter((student) => {
+    return student.age >= 30
+
+    // if(student.age >= 30){
+    //     return true
+    // } else {
+    //     return false
+    // }
+
+
+});
+console.log(studentsOver30);
+
+
+// Trova lo studente con id 4
+const studentId4 = students.find((student) => {
+    return student.id === 4
 });
 
-//RISULTATO: { id: 3, name: 'Clelia', age: 34 }
-
-console.log(myPerson);
-
-
-// versione con for tradizionale
-let myPerson2;
-
-for (let i = 0; i < people.length; i++) {
-    const person2 = people[i];
-
-    if (person2.id === 3) {
-        myPerson2 = person2;
-    }
-}
-
-console.log(myPerson2);
+console.log(studentId4);
