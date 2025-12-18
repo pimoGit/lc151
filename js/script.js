a38b976becfb438379088e7674afa887ed935ee8
Simone <info@simoneicardi.com>
2025-12-18 10:34:00 +0100

    es. filter con for tradizionale

--- js/script.js
+++ js/script.js
@@ -4,8 +4,22 @@ const students = [
     { id: 1, name: 'Ginevra', year: 1 }
 ];
 
-const firstYearStudents = students.filter((studentItem) => {
+const firstYearStudents = students.filter(studentItem => {
     return studentItem.year === 1
 });
 
 console.log(firstYearStudents, students);
+
+
+// for tradizionale
+const firstYearStudents2 = [];
+
+for (let i = 0; i < students.length; i++) {
+    const studentItem2 = students[i];
+
+    if (studentItem2.year === 1) {
+        firstYearStudents2.push(studentItem2);
+    }
+}
+
+console.log(firstYearStudents2);
