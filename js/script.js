d1eab183fa9d70dae6ce3c12db1b663372d2bf17
Simone <info@simoneicardi.com>
2025-12-19 11:52:45 +0100

    es. primo utilizzo chiamata ajax a API

--- js/script.js
+++ js/script.js
@@ -1,39 +1,26 @@
-const people = [
-    {
-        firstName: 'Marco',
-        lastName: 'Lanci',
-        age: 35,
-    },
-    {
-        firstName: 'Antonio',
-        lastName: 'Nicodemo',
-        age: 16,
-    },
-    {
-        firstName: 'Luigi',
-        lastName: 'Versi',
-        age: 14,
-    },
-    {
-        firstName: 'Mario',
-        lastName: 'Rossi',
-        age: 18,
-    },
-];
+// setup iniziale
+const myOutput = document.getElementById("output");
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/int";
+
+// gestiamo la chiamata ajax con axios
+axios.get(endpoint)
+    .then(response => {
+        //codice da eseguire in caso di successo
+        const result = response.data;
+
+        console.log(response, result);
+
+        // facciamo output in page
+        myOutput.innerText = result.response;
 
 
-const drivingPeople = people.map(person => {
-    const { firstName, lastName, age } = person;
-    const canDrive = age >= 18 ? 'può' : 'non può';
-
-    // let canDrive2 = undefined;
-    // if (age >= 18) {
-    //     canDrive2 = 'può';
-    // } else {
-    //     canDrive2 = 'non può';
-    // }
-
-    return `${firstName} ${lastName} ${canDrive} guidare`;
-})
-
-console.log(drivingPeople);
+    })
+    .catch(error => {
+        // codice da eseguire in caso di errore
+        console.error(error.message)
+    })
+    .finally(() => {
+        // codice da eserguire a prescindere dall'esito
+        // (ad esempio si può fermare un loader!)
+        console.log("fatto tutto, fine della chiamata");
+    })
