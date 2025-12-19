1c5648c7d321fb67f0a2a2f44ef34219ea70e895
Simone <info@simoneicardi.com>
2025-12-19 12:20:23 +0100

    es. gioco dadi con richiesta ajax ad API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // setup iniziale
+// selezione degli elementi del DOM
 const myOutput = document.getElementById("output");
-const endpoint = "https://flynn.boolean.careers/exercises/api/random/int";
+const outputPlayUno = document.getElementById("playeruno");
+const outputPlayDue = document.getElementById("playerdue");
+
+
+// setup dei ref a valori
+const numDice = 2;
+const numMin = 1;
+const numMax = 6;
+const endpoint = `https://flynn.boolean.careers/exercises/api/array/integers?min=${numMin}&max=${numMax}&items=${numDice}`;
 
 // gestiamo la chiamata ajax con axios
 axios.get(endpoint)
@@ -10,9 +19,27 @@ axios.get(endpoint)
 
         console.log(response, result);
 
-        // facciamo output in page
-        myOutput.innerText = result.response;
+        // estrapoliamo i due numeri e li assegniamo agli utenti
+        // const playerUnoNum = result.response[0];
+        // const playerDueNum = result.response[1];
+        const [playerUnoNum, playerDueNum] = result.response;
+        // const [playerUnoNum, playerDueNum] = [2, 2];
 
+        console.log(playerUnoNum, playerDueNum);
+
+        // settiamo var di messaggio
+        let message = "pareggio";
+
+        // output dei numeri
+        outputPlayUno.innerText = playerUnoNum;
+        outputPlayDue.innerText = playerDueNum;
+
+        // condizioni per output messaggio vincita
+        if (playerUnoNum > playerDueNum) message = "Ha vinto il primo giocatore";
+        else if (playerUnoNum < playerDueNum) message = "Ha vinto il secondo giocatore";
+
+        // output messsaggio finale
+        myOutput.innerText = message;
 
     })
     .catch(error => {
