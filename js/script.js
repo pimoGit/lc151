e4d7cbbbbd34b6ee0dd904dd2f72e066406fc176
Simone <info@simoneicardi.com>
2026-01-08 10:53:41 +0100

    es. genera dinamica page da ajax

--- js/script.js
+++ js/script.js
@@ -1,82 +1,61 @@
-// setup iniziale
-// selezione degli elementi del DOM
-const myOutput = document.getElementById("output");
-const outputPlayUno = document.getElementById("playeruno");
-const outputPlayDue = document.getElementById("playerdue");
-const playButton = document.querySelector("button");
+// seleziono elemento di container output
+const outputCont = document.getElementById("container");
+// console.log(outputCont);
 
-
-// setup dei ref a valori
-const numDice = 2;
-const numMin = 1;
-const numMax = 6;
-const endpoint = `https://flynn.boolean.careers/exercises/api/array/integers?min=${numMin}&max=${numMax}&items=${numDice}`;
+// creiamo ref a endpoint
+const endpoint = "https://jsonplaceholder.typicode.com/posts?_limit=15";
 
 
 
-// avvio gioco
-playDice();
+// creiamo la chiamata ajax all'endpoint
+axios.get(endpoint)
+    .then(response => {
+        // otteniamo l'array di oggetti dall'API
+        const posts = response.data;
+        // console.log(posts);
 
-// se utente clicca rigioca
-playButton.addEventListener("click", playDice);
+        // var di accumulo stringa output
+        let postsOutput = "";
+
+        // cicliamo l'array per estrapolare le info
+        posts.forEach((post, index) => {
+
+            // destrutturiamo l'oggeto
+            // const title = post.title;
+            // const body = post.body;
+
+            const { title, body } = post;
+
+            // console.log("il valore del titolo è :", title, "il valore del testo è :", body);
+
+            // valorizziamo la variabile di accumulo di output (stringa)
+            postsOutput += `
+            <div class="card">
+                <h2>${title}</h2>
+                <p>${body}</p>
+            </div>`;
+
+            console.log("alla iterazione numero", index, postsOutput);
+
+        });
+
+        // inseriamo in pagina le card accumulate
+        outputCont.innerHTML = postsOutput;
+
+
+    })
+    .catch()
 
 
 
 
-// Funzione gioco
-function playDice() {
 
-    // ripuliamo output dadi
-    myOutput.innerHTML = "";
-    outputPlayUno.innerHTML = "";
-    outputPlayDue.innerHTML = "";
 
-    // gestiamo la chiamata ajax con axios
-    axios.get(endpoint)
-        .then(response => {
-            //codice da eseguire in caso di successo
-            const result = response.data;
 
-            console.log(response, result);
 
-            // estrapoliamo i due numeri e li assegniamo agli utenti
-            // const playerUnoNum = result.response[0];
-            // const playerDueNum = result.response[1];
-            const [playerUnoNum, playerDueNum] = result.response;
-            // const [playerUnoNum, playerDueNum] = [2, 2];
+// FUNZIONI
+// function cardGenerator(){
 
-            console.log(playerUnoNum, playerDueNum);
 
-            // settiamo var di messaggio
-            let message = "pareggio";
 
-            // creiamo le img
-            const img1 = document.createElement("img");
-            img1.src = `img/${playerUnoNum}.png`;
-
-            const img2 = document.createElement("img");
-            img2.src = `img/${playerDueNum}.png`;
-
-            // output dei numeri
-            outputPlayUno.appendChild(img1);
-            outputPlayDue.appendChild(img2);
-
-            // condizioni per output messaggio vincita
-            if (playerUnoNum > playerDueNum) message = "Ha vinto il primo giocatore";
-            else if (playerUnoNum < playerDueNum) message = "Ha vinto il secondo giocatore";
-
-            // output messsaggio finale
-            myOutput.innerText = message;
-
-        })
-        .catch(error => {
-            // codice da eseguire in caso di errore
-            console.error(error.message)
-        })
-        .finally(() => {
-            // codice da eserguire a prescindere dall'esito
-            // (ad esempio si può fermare un loader!)
-            console.log("fatto tutto, fine della chiamata");
-        })
-
-}
+// }
